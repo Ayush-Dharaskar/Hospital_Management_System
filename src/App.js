0522f6c9c9e2153400e164af62f73f9d645aaa2e
saicharan1342 <ssc25082004@gmail.com>
2024-03-30 20:37:51 +0530

    Fix bug in login functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,125 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // State to hold the input text
+  const [inputText, setInputText] = useState('');
+  const [nj, setnj] = useState([]);
+  const [nj1, setnj1] = useState([]);
+
+  // Function to handle button click and send text to delete item
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:5000/add_text_to_postgresql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ inputText }),
+      });
+      if (response.ok) {
+        alert('Text added to PostgreSQL successfully!');
+        setInputText('');
+        
+        console.log(inputText);
+      } else {
+        alert('Failed to add text to PostgreSQL. Please try again later.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleSubmit1 = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:5000/fetch', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ inputText }),
+      });
+      const data = await response.json();
+      setnj(data.item);
+      if (response.ok) {
+        alert('Text added to PostgreSQL successfully!');
+        setInputText('');
+        
+        console.log(inputText);
+      } else {
+        alert('Failed to add text to PostgreSQL. Please try again later.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  const handleSubmit2 = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:5000/patientextract', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ inputText }),
+      });
+      const data = await response.json();
+      setnj(data.patient);
+      setnj1(data.bmi);
+      if (response.ok) {
+        alert('PAtient details fetched successfully!');
+        setInputText('');
+        
+        console.log(inputText);
+      } else {
+        alert('Failed to add text to PostgreSQL. Please try again later.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  
+
+  // Function to handle input change
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value;
+    console.log(typeof inputValue);
+    setInputText(inputValue);
+  };
+
   return (
-    <div className="App">
+    <div className="App"> 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input 
+          id='inopp'
+          type="text"
+          onChange={handleInputChange}
+          placeholder="Enter item name"
+          className="App-input"
+        />
+        <button className="App-button" onClick={handleSubmit}>
+          Deete Item
+        </button>
+        <ul>
+          {nj.map((item, index) => (
+            <div>
+              <li>{'Pid:'+item.patient_id}</li>
+              <li>{'height:'+item.height}</li>
+              <li>{'weight:'+item.weight}</li>
+              <li>{'Name:'+item.patient_name}</li>
+              <li>{nj1}</li>
+            </div>
+          ))}
+        </ul>
+        <button className="App-button" onClick={handleSubmit1}>
+          Fetch
+        </button>
+         <button className="App-button" onClick={handleSubmit2}>
+          Fetch P
+        </button>
       </header>
     </div>
   );
